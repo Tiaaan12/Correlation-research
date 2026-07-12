f2afb8e8134264f8e6ef74ce69985e773339f650
Tiaaan12 <christiandevera232@gmail.com>
2026-07-12 21:43:40 +0800

    minor changes

--- src/visualization.py
+++ src/visualization.py
@@ -121,7 +121,7 @@ def plot_wellbeing_gender_boxplot(df):
     sns.set_style("whitegrid")
     plt.figure(figsize=(8, 5))
     custom_palette = {"Male": "#5dade2", "Female": "#ec7063"} 
-    
+    ri
     sns.boxplot(
     data=df,
     x="gender",
