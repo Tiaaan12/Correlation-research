42160db01cff20577a1ff72d4c8d49d379e216fc
Tiaaan12 <christiandevera232@gmail.com>
2026-07-14 10:10:33 +0800

    improt statistics functions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,2 @@
-from data cleaning import load_data, clean, data, encode_data
+from data_cleaning import load_data, clean, data, encode_data
+from statistics import descriptive_statistics, correlation_analysis, linear_regression
